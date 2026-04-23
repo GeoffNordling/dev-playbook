bca617ebd70ba2bf6e031e78f15433d919f1d84b
Geoff Nordling <thegnord@gmail.com>
2026-04-23 16:14:15 -0400

    gp alias

--- dotfiles/.bashrc.d/aliases.sh
+++ dotfiles/.bashrc.d/aliases.sh
@@ -4,6 +4,7 @@ alias work='cd /home/geoff/workspace'
 
 alias sso='aws sso login --sso-session geoff-sso'
 
+alias gp='git pull'
 alias gb='git branch'
 alias gs='git status'
 alias gaa='git add --all'
